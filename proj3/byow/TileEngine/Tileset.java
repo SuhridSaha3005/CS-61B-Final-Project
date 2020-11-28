b929bec34450c7c2e31e03a10c77c31594318063
Suhrid Saha <suhrid@berkeley.edu>
2020-11-27 22:38:19 -0800

    new Tiles for avatars

--- proj3/byow/TileEngine/Tileset.java
+++ proj3/byow/TileEngine/Tileset.java
@@ -33,6 +33,8 @@ public class Tileset {
     public static final TETile SAND = new TETile('▒', Color.yellow, Color.black, "sand");
     public static final TETile MOUNTAIN = new TETile('▲', Color.gray, Color.black, "mountain");
     public static final TETile TREE = new TETile('♠', Color.green, Color.black, "tree");
+    public static final TETile PLAYER = new TETile('☺', Color.white, Color.black, "player");
+    public static final TETile GHOST = new TETile('☠', Color.yellow, Color.black, "ghost");
 }
 
 

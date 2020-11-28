6cab96c3294a7fb4930134b9064ace0dd3bb49b2
DarkHanSolo <shrihan@berkeley.edu>
2020-11-28 23:49:31 +0530

    VERY MINOR change to Avatar, so that we can walk through lamps (added one instance variable)

--- proj3/byow/Core/Avatar.java
+++ proj3/byow/Core/Avatar.java
@@ -11,12 +11,14 @@ public class Avatar {
     private final TETile[][] world;
     private final TETile appearance;
     private XYPosn position;
+    private TETile aboveTile;
 
     public Avatar(TETile[][] world, TETile appearance, XYPosn position) {
         this.world = world;
         this.appearance = appearance;
         world[position.getX()][position.getY()] = appearance;
         this.position = position;
+        this.aboveTile = Tileset.FLOOR;
     }
 
     private XYPosn up() {
@@ -55,31 +57,38 @@ public class Avatar {
         if (key == 'w') {
             XYPosn up = up();
             if (up != null && world[up.getX()][up.getY()] != Tileset.WALL) {
-                world[position.getX()][position.getY()] = Tileset.FLOOR;
+                world[position.getX()][position.getY()] = aboveTile;
+                aboveTile = world[up.getX()][up.getY()];
                 position = up;
                 world[up.getX()][up.getY()] = appearance;
             }
         } else if (key == 's') {
             XYPosn down = down();
             if (down != null && world[down.getX()][down.getY()] != Tileset.WALL) {
-                world[position.getX()][position.getY()] = Tileset.FLOOR;
+                world[position.getX()][position.getY()] = aboveTile;
+                aboveTile = world[down.getX()][down.getY()];
                 position = down;
                 world[down.getX()][down.getY()] = appearance;
             }
         } else if (key == 'd') {
             XYPosn right = right();
             if (right != null && world[right.getX()][right.getY()] != Tileset.WALL) {
-                world[position.getX()][position.getY()] = Tileset.FLOOR;
+                world[position.getX()][position.getY()] = aboveTile;
+                aboveTile = world[right.getX()][right.getY()];
                 position = right;
                 world[right.getX()][right.getY()] = appearance;
             }
         } else if (key == 'a') {
             XYPosn left = left();
             if (left != null && world[left.getX()][left.getY()] != Tileset.WALL) {
-                world[position.getX()][position.getY()] = Tileset.FLOOR;
+                world[position.getX()][position.getY()] = aboveTile;
+                aboveTile = world[left.getX()][left.getY()];
                 position = left;
                 world[left.getX()][left.getY()] = appearance;
             }
+        }
+        if (aboveTile.equals(Tileset.GHOST) || aboveTile.equals(Tileset.PLAYER)) {
+            aboveTile = Tileset.FLOOR;
         }
     }
 

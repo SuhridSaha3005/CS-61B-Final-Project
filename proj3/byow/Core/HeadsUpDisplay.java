78a95589e54cafd79a41e2bd0c23f3458062e016
Suhrid Saha <suhrid@berkeley.edu>
2020-11-28 18:22:08 -0800

    Changed distance

--- proj3/byow/Core/HeadsUpDisplay.java
+++ proj3/byow/Core/HeadsUpDisplay.java
@@ -3,12 +3,14 @@ package byow.Core;
 
 import edu.princeton.cs.introcs.StdDraw;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class HeadsUpDisplay {
     int keys;
     int width;
     int height;
+    static DecimalFormat df = new DecimalFormat("0.00");
 
     public HeadsUpDisplay(int k, int w, int h) {
         keys = k;
@@ -19,23 +21,18 @@ public class HeadsUpDisplay {
     public void update(Avatar player, ArrayList<Avatar> ghost) {
         double textHeight = height  - 1.5;
         StdDraw.text(10, textHeight, "Keys Remaining: " + keys);
-        StdDraw.text(width - 15, textHeight, "Distance To Closest Ghost: " + getMinGhostDist(player, ghost));
+        StdDraw.text(width - 15, textHeight, "Distance To Closest Ghost: "
+                + df.format(getMinGhostDist(player, ghost)));
     }
 
-    private int getMinGhostDist(Avatar player, ArrayList<Avatar> ghosts) {
-        int min = width + height + 200;
+    private double getMinGhostDist(Avatar player, ArrayList<Avatar> ghosts) {
+        double min = Double.POSITIVE_INFINITY;
         for (Avatar g: ghosts) {
-            min = Math.min(manhattan(player.getPosn(), g.getPosn()), min);
+            if (Avatar.distance(player,g) < min) {
+                min = Avatar.distance(player,g);
+            }
         }
         return min;
-    }
-
-    private int manhattan(XYPosn player, XYPosn ghost) {
-        return manhattan(player.getX(), player.getY(), ghost.getX(), ghost.getY());
-    }
-
-    public int manhattan(int playerX, int playerY, int ghostX, int ghostY) {
-        return Math.abs(playerX - ghostX) + Math.abs(playerY - ghostY);
     }
 
 }
